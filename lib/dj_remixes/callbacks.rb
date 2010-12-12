e6a24b9a31bca42dac731cf9f6057d65ead9accb
Brent Kirby <brent@kurbmedia.com>
2010-12-12 13:49:18 -0500

    support for time updates not responding to "touch"

--- lib/dj_remixes/callbacks.rb
+++ lib/dj_remixes/callbacks.rb
@@ -3,14 +3,14 @@ module DJ
     
     def before(job)
       self.dj_object = job
-      job.touch(:started_at)
+      (job.respond_to?(:touch) ? job.touch(:started_at) : job.update_attributes(:started_at => Time.now))
     end
     
     def after(job)
     end
     
     def success(job)
-      job.touch(:finished_at)
+      (job.respond_to?(:touch) ? job.touch(:finished_at) : job.update_attributes(:finished_at => Time.now))
       self.enqueue_again if self.respond_to?(:enqueue_again)
     end
     
